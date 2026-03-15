47d4c8768aca88e87347e2d086f1d9482a2d95a1
BohdanStarter <bogdan2cs2@gmail.com>
2026-03-15 04:32:50 +0200

    Initial commit

---
+++ email_form.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, redirect
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/send", methods=["POST"])
+def send():
+    name = request.form["name"]
+    email = request.form["email"]
+    message = request.form["message"]
+
+    requests.post(
+        "https://api.mailgun.net/v3/sandbox976ed9dc149a49c48c73280d00e81f7b.mailgun.org/messages",
+        auth=("api", os.getenv("API_KEY")),
+        data={
+            "from": f"{name} <postmaster@sandbox976ed9dc149a49c48c73280d00e81f7b.mailgun.org>",
+            "to": "bogdan2cs2@gmail.com",
+            "subject": "New message from website (Contact Me Form)",
+            "text": f"Name: {name}\nEmail: {email}\nMessage:\n{message}"
+        }
+    )
+
+    return redirect("/")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.route("/toMain", methods=["GET"])
+def not_found_transition():
+    print("Redirecting...")
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
